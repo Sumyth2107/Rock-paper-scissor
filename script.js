1c1578e56292cd82c3be95a18bafca59b8bd410b
Sumyth2107 <sumith2107@gmail.com>
2024-07-06 17:39:03 +0530

    added ui to the game

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
-console.log("hi");
+var playerScore = 0;
+var computerScore = 0;
 
-function getComputerChoice(){
-    var value =Math.random();
-    if(value<0.35){
-        return "Rock";
+var resultDiv = document.getElementById('result');
+var scoreDiv = document.getElementById('score');
+
+document.getElementById('rock').addEventListener('click', function() {
+    playRound('rock');
+});
+document.getElementById('paper').addEventListener('click', function() {
+    playRound('paper');
+});
+document.getElementById('scissors').addEventListener('click', function() {
+    playRound('scissors');
+});
+
+function computerPlay() {
+    var choices = ['rock', 'paper', 'scissors'];
+    var randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function playRound(playerSelection) {
+    var computerSelection = computerPlay();
+    var result = '';
+
+    if (playerSelection === computerSelection) {
+        result = "It's a tie!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        result = 'You win! ' + playerSelection + ' beats ' + computerSelection + '.';
+    } else {
+        computerScore++;
+        result = 'You lose! ' + computerSelection + ' beats ' + playerSelection + '.';
     }
-    else if(value<0.65){
-        return "paper";
+
+    resultDiv.textContent = result;
+    scoreDiv.textContent = 'Player Score: ' + playerScore + ' - Computer Score: ' + computerScore;
+
+    if (playerScore === 5) {
+        alert('Congratulations! You won the game!');
+        resetGame();
+    } else if (computerScore === 5) {
+        alert('Sorry, you lost the game. Try again!');
+        resetGame();
     }
-    else{
-        return "Scissor";
-    }
-}
-function getHumanChoice(){
-let sign = prompt(" enter rock paper or scissor");
-return sign;
-
-}
-function winner(player,computer){
-if ( player==computer){
-    return "draw";
 }
 
-else if( (player==="paper" && computer==="rock" )
-    ||(player==="rock" && computer==="Scissor" )||(player==="scissor" && computer==="paper" )
-)
-return " you win ";
-else {
-    return "you lost";
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    resultDiv.textContent = '';
+    scoreDiv.textContent = '';
 }
-}
-
-function play(){
-    var player=getHumanChoice();
-    var computer=getComputerChoice();
-    
-    alert(winner(player,computer));
-}
-play();
-
